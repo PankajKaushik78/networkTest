96dfdc7e562f0553adb6511186b8ba0d7fb7b9e6
Pankaj Kaushik <kaushikpankaj78@gmail.com>
2023-05-04 16:53:41 +0530

    test

---
+++ Sources/NetworkCapture/models/NetworkRequest.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkRequest.swift
+//  test
+//
+//  Created by Pankaj Kaushik on 04/05/23.
+//
+
+import Foundation
---
+++ Sources/NetworkCapture/models/NetworkResponse.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkResponse.swift
+//  test
+//
+//  Created by Pankaj Kaushik on 04/05/23.
+//
+
+import Foundation
